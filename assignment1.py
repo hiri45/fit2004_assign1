c848aa33a50e9f2f6490c831df7377c54f2da8d5
Hirun <hhet0002@student.monash.edu>
2022-08-15 12:49:04 +1000

    character set

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+"""
+High level description about the function and the approach you
+have undertaken.
+:Input:
+argv1:
+argv2:
+:Output, return or postcondition:
+:Time complexity:
+:Aux space complexity:
+"""
+def analyze(results, roster, score):
+    char_set = []
+    for _ in range(roster):
+        char_set.append(chr(ord('A')+c))
